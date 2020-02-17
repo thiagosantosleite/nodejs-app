b4ea47851b385966bc7ccfd31cc1b29e88790182
root <root@ip-172-31-94-178.ec2.internal>
2020-02-17 01:13:19 +0000

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update -y
 
+### install node lts
 wget https://nodejs.org/dist/v12.16.0/node-v12.16.0-linux-x64.tar.xz
 VERSION=v12.16.0
 DISTRO=linux-x64
@@ -15,16 +16,25 @@ EOF
 
 cat ~/.profile
 . ~/.profile
+
+## install pm2 and start app using one processo for each cpu
 npm install pm2 -g
-
 cd src
-pm2 start app.js -i max
+pm2 start app.js -i max 
 
 
-
+## install python3 for parser and nginx for the reverse proxy
 amazon-linux-extras install nginx1.12 -y
 amazon-linux-extras install python3 -y
 
+
+## setup ssl for nginx, self signed certificate
+mkdir -p /usr/local/nginx/ssl
+cd /usr/local/nginx/ssl
+if [[ -z $SITE_IP ]]; then SITE_IP=18.207.186.2; echo "SITE_IP is not configured, so using 18.207.186.2"; fi;
+openssl req -x509 -nodes -newkey rsa:2048 -keyout cert.key -out cert.crt -days 365 -subj "/C=BR/ST=Rio Grande do Sul/L=POA/O=xxxxx/CN=$SITE_IP"
+
+## setup reverse proxy
 cat > /etc/nginx/nginx.conf << EOF
 user nginx;
 worker_processes auto;
@@ -60,6 +70,20 @@ http {
          proxy_set_header   Host localhost;
          proxy_pass         http://localhost:3000;
      }
+   }
+   server {
+     listen 443 ssl;
+     server_name         localhost;
+     ssl_certificate     /usr/local/nginx/ssl/cert.crt;
+     ssl_certificate_key /usr/local/nginx/ssl/cert.key;
+     ssl_protocols       SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+     ssl_ciphers         HIGH:!aNULL:!MD5;
+     location / {
+         proxy_set_header   X-Forwarded-For localhost;
+         proxy_set_header   Host localhost;
+         proxy_pass         http://localhost:3000;
+     }
+
    }
 }
 EOF
